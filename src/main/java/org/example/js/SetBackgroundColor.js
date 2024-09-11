9ed0089de84c216d8922fa758546ed55cf4de4a2
kasperkhansen <kasper1999.hansen@gmail.com>
2024-09-11 08:53:09 +0200

    intit commit

---
+++ src/main/java/org/example/js/SetBackgroundColor.js
@@ -0,0 +1,85 @@
+
+//-------------------------konstanter og variabler----------------------------------
+let inp = document.querySelector(".inpColor");
+console.log(inp);
+
+
+let pbCol = document.querySelector(".pbSetColor");
+console.log(pbCol);
+
+
+pbCol.textContent = "Tryk mig for set color";
+
+
+let bdy = document.querySelector("body");
+console.log(bdy);
+
+
+const inpColorValue = document.querySelector(".inpColorValue");
+console.log(inpColorValue);
+
+
+const inpColorPicker = document.getElementById("inpColorPicker");
+console.log(inpColorPicker);
+
+
+const pTags = document.getElementsByTagName('p');
+console.log(pTags);
+
+
+let increaseBtn = document.querySelector(".increaseFontSize")
+let decreaseBtn = document.querySelector(".decreaseFontSize")
+
+
+const pArray = Array.from(pTags)
+pArray.forEach(increaseFont);
+
+
+//------------------------------Funktioner-------------------------------------
+
+function setBackgroundColor() {
+    let col = inp.value;
+    console.log(col);
+    bdy.style.backgroundColor = col;
+    pbCol.style.color = col;
+    inpColorValue.value = bdy.style.backgroundColor;
+}
+
+
+function useColorPicker() {
+    let col = inpColorPicker.value;
+    console.log(col)
+    bdy.style.backgroundColor = col;
+    pbCol.style.color = col;
+    inpColorValue.value = bdy.style.backgroundColor;
+}
+
+
+function increaseFont(element) {
+    pArray.forEach(p => {
+        let currentSize = window.getComputedStyle(p).fontSize;
+        let newSize = parseFloat(currentSize) + 1; // Øger med 1 pixel
+        p.style.fontSize = newSize + 'px';
+    });
+}
+
+
+function decreaseFont() {
+    pArray.forEach(p => {
+        let currentSize = window.getComputedStyle(p).fontSize;
+        let newSize = parseFloat(currentSize) - 1; // Mindsker med 1 pixel
+        p.style.fontSize = newSize + 'px';
+    });
+}
+
+//-----------------------------EventListeners---------------------------------------------
+
+increaseBtn.addEventListener('click', increaseFont);
+decreaseBtn.addEventListener('click', decreaseFont);
+
+document.addEventListener("keyup", setBackgroundColor)
+inpColorPicker.addEventListener('input', useColorPicker)
+
+pbCol.addEventListener('click', setBackgroundColor)
+
+
